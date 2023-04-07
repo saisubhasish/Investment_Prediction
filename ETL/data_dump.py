59e7b2de4fcfcd5c1d41580f38985e2a1a83f1b6
saisubahasish <saisubhasishrout777@gmail.com>
2023-04-07 18:50:05 +0530

    Updated data_dump.py

--- ETL/data_dump.py
+++ ETL/data_dump.py
@@ -1,9 +1,11 @@
 import os
+import sys
 import json
 import pymongo
 import pandas as pd
 from investment_prediction.config import mongo_client
 from investment_prediction.logger import logging
+from investment_prediction.exception import InvestmentPredictionException
 from investment_prediction.config import preprocessed_file_path, DATABASE_NAME, COLLECTION_NAME_br, COLLECTION_NAME_itc, COLLECTION_NAME_rel, COLLECTION_NAME_tatam, COLLECTION_NAME_tcs
 
 
@@ -36,31 +38,39 @@ class Data_Loading:
         logging.info("Preparing list of collections")
         collection_list = [COLLECTION_NAME_br, COLLECTION_NAME_itc, COLLECTION_NAME_rel, COLLECTION_NAME_tatam, COLLECTION_NAME_tcs]
 
-        logging.info("Resetting index")
-        list(map(lambda df: df.reset_index(drop=True,inplace=True), df_list))
+        try:
+            logging.info('Updating records to insert data to mongoDB')
+            logging.info("Resetting index")
+            list(map(lambda df: df.reset_index(drop=True,inplace=True), df_list))
 
-        logging.info("Converting the 'Date' column datatype")
-        for df in df_list:
-            df['Date'] = pd.to_datetime(df['Date'])
+            logging.info("Converting the 'Date' column datatype")
+            for df in df_list:
+                df['Date'] = pd.to_datetime(df['Date'])
 
-        logging.info("Convert dataframe to json so that we can dump these record in mongo db")
-        logging.info("Each record will represent one row")
-        json_record_br = df_br.to_dict('records')
-        json_record_itc = df_itc.to_dict('records')
-        json_record_rel = df_rel.to_dict('records')
-        json_record_tatam = df_tatam.to_dict('records')
-        json_record_tcs = df_tcs.to_dict('records')
+            logging.info("Convert dataframe to json type(dict) so that we can dump these record in mongo db")
+            logging.info("Each record will represent one row")
+            json_record_br = df_br.to_dict('records')
+            json_record_itc = df_itc.to_dict('records')
+            json_record_rel = df_rel.to_dict('records')
+            json_record_tatam = df_tatam.to_dict('records')
+            json_record_tcs = df_tcs.to_dict('records')
+
+        except Exception as e:
+            raise InvestmentPredictionException(e, sys)
 
         logging.info("Preparing list of json records")
         json_record_list = [json_record_br, json_record_itc, json_record_rel, json_record_tatam, json_record_tcs]
 
         print(json_record_br[1])
 
-        logging.info("inserting converted json record to mongo db")
-        i = 0
-        for collection in collection_list:
-            mongo_client[DATABASE_NAME][collection].insert_many(json_record_list[i])
-            i+=1
+        try:
+            logging.info("inserting converted json record to mongo db")
+            i = 0
+            for collection in collection_list:
+                mongo_client[DATABASE_NAME][collection].insert_many(json_record_list[i])
+                i+=1
+        except Exception as e:
+            raise InvestmentPredictionException(e, sys)
 
     # Function call
 Data_Loading.dump(preprocessed_file_path, DATABASE_NAME, COLLECTION_NAME_br, COLLECTION_NAME_itc, COLLECTION_NAME_rel, COLLECTION_NAME_tatam, COLLECTION_NAME_tcs)
